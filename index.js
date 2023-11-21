ad0981eb112bb6676cc774aba28751d9d4f82a3c
Harsha S <sharsha315@gmail.com>
2023-11-21 16:47:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// Import Apollo Server and schema 
+const { ApolloServer } = require("apollo-server");
+const { importSchema } = require("graphql-import");
+
+// Import custom data source
+const EtherDataSource = require("./datasource/ethDatasource"); 
+
+// Import schema
+const typeDefs = importSchema("./schema.graphql");
+
+// Load environment variables
+require("dotenv").config();
+
+// Define resolvers
+const resolvers = {
+  Query: {
+    etherBalanceByAddress: (root, _args, { dataSources }) => 
+      dataSources.ethDataSource.etherBalanceByAddress(),
+
+    totalSupplyOfEther: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.totalSupplyOfEther(),
+
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    blockConfirmationTime: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+// Create Apollo Server instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    ethDataSource: new EtherDataSource(), 
+  }),
+});
+
+// Set timeout and start server 
+server.timeout = 0;
+server.listen("9000").then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`); 
+});
